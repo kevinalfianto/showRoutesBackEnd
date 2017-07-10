1e726017762adf20817fdc26fcd7b649d9de1759
kevinalfianto <kevinalfianto@gmail.com>
2017-07-10 11:57:48 +0700

    change procfile

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var Sequelize = require('sequelize');
 
 var index = require('./routes/index');
 var create = require('./routes/create');
